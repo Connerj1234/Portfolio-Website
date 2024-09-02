ed5375ef0813c0d2a57118656e69a14f542352c5
Connerj1234 <mailtoclyjams@gmail.com>
2024-09-02 01:21:24 -0400

    Completed Initial Draft of Portfolio Website

---
+++ script.js
@@ -0,0 +1,43 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  const backToTopBtn = document.getElementById("backToTopBtn");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+  e.preventDefault()
+  emailjs.sendForm('service_3145k1q','template_07pmf12','#contact-form','Cwh2IHGKHlUtxZeSW')
+    .then(() =>{
+        contactMessage.textContent = 'Message sent successfully!✅'
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset()
+    }, () => {
+      contactMessage.textContent = 'Message not sent (service error).❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
